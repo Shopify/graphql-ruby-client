b7e02e274384d4ae3c1fecaadbf17c71433235a2
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2016-08-10 16:27:47 -0400

    Cleanup gemspec and Rakefile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,6 @@
+require 'graphql_client'
 require 'pry'
 require 'json'
-require 'vcr'
-# require 'webmock/minitest'
-
 require 'simplecov'
+
 SimpleCov.start
-
-require 'graphql_client'
-
-# VCR.configure do |config|
-#   config.cassette_library_dir = "fixtures/vcr_cassettes"
-#   config.hook_into :webmock
-# end
