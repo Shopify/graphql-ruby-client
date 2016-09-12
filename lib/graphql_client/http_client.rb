9d8d047ef4220ca028d60e234ecb2c580d89af37
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2016-09-12 17:55:09 -0400

    Add query and raw_query methods

--- lib/graphql_client/http_client.rb
+++ lib/graphql_client/http_client.rb
@@ -25,11 +25,22 @@ module GraphQL
         end
       end
 
+      def query(query)
+        req = Request.new(self)
+        req.send_request(query.to_query)
+      end
+
+      def raw_query(query_string)
+        req = Request.new(self)
+        req.send_request(query_string)
+      end
+
       private
 
       def define_field_accessors
         query_root = @schema.query_root
         fields = query_root.fields.scalars + query_root.fields.objects
+
         fields.each do |name, field|
           define_singleton_method(name) do |**arguments|
             ObjectProxy.new(field: field, client: self, **arguments)
--- lib/graphql_client/response.rb
+++ lib/graphql_client/response.rb
@@ -3,25 +3,18 @@ require 'json'
 module GraphQL
   module Client
     class Response
-      ResponseError = Class.new(StandardError)
+      MalformedError = Class.new(StandardError)
+
       attr_reader :data
 
-      def initialize(request, response)
-        @page = 1
-        @response = response
-        @request = request
+      def initialize(response_body)
+        parsed_response = JSON.parse(response_body)
 
-        parsed_response = JSON.parse(@response)
         if parsed_response.key?('errors')
-          raise ResponseError, "Malformed response - #{response}"
+          raise MalformedError, "Malformed response: #{parsed_response['errors']}"
         end
 
-        @data = parsed_response['data']
-      end
-
-      def object
-        type_name = @data.keys.first
-        @data[type_name]
+        @data = parsed_response.fetch('data')
       end
     end
   end
