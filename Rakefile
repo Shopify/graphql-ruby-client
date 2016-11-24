c7a7aea5a83fbb08cc423b4f0eccffea04d891cd
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2016-11-24 16:48:24 -0500

    Remove proxies + AR-style API

--- Rakefile
+++ Rakefile
@@ -6,26 +6,7 @@ Rake::TestTask.new do |t|
   t.libs << 'test'
 
   t.test_files = FileList.new do |test_files|
-    test_files.include('test/*_test.rb')
-    test_files.include('test/graphql_client/**/*_test.rb')
-
-    if ENV['MERCHANT_USERNAME'] && ENV['MERCHANT_PASSWORD']
-      test_files.include('test/integration/merchant_client_test.rb')
-    else
-      puts "Skipping Merchant client tests"
-    end
-
-    if ENV['MERCHANT_TOKEN']
-      test_files.include('test/integration/merchant_channel_test.rb')
-    else
-      puts "Skipping Merchant channel tests"
-    end
-
-    if ENV['STOREFRONT_TOKEN']
-      test_files.include('test/integration/storefront*_test.rb')
-    else
-      puts "Skipping StoreFront tests"
-    end
+    test_files.include('test/**/*_test.rb')
   end
 end
 
--- test/graphql_client/http_client_test.rb
+++ test/graphql_client/http_client_test.rb
@@ -14,7 +14,7 @@ module GraphQL
         end
       end
 
-      def test_query_calls_adapter_request_with_query_builder_instance
+      def test_query_calls_adapter_request_with_query_builder_instance_and_creates_a_graph_object
         schema_string = File.read(fixture_path('merchant_schema.json'))
         schema = GraphQLSchema.new(schema_string)
         config = Config.new(url: 'http://example.com')
@@ -23,12 +23,22 @@ module GraphQL
         query.expect(:to_query, 'query shopQuery { shop }')
 
         adapter = Minitest::Mock.new
-        adapter.expect(:request, nil, ['query shopQuery { shop }', operation_name: 'shopQuery', variables: {}])
+        adapter.expect(:request, Response.new('{}'), [
+          'query shopQuery { shop }',
+          operation_name: 'shopQuery',
+          variables: {}
+        ])
 
-        client = Base.new(schema, config: config, adapter: adapter)
-        client.query(query, operation_name: 'shopQuery')
+        mock = Minitest::Mock.new
+        mock.expect(:call, nil, [data: nil, query: query])
 
-        query.verify
+        GraphObject.stub(:new, mock) do
+          client = Base.new(schema, config: config, adapter: adapter)
+          client.query(query, operation_name: 'shopQuery')
+
+          mock.verify
+          query.verify
+        end
       end
 
       def test_raw_query_calls_adapter_request_with_query_string
